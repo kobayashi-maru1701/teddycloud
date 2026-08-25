74495d55e248c1c1f80d6fa910f8f3f9257bbbbe
kobayashi-maru1701 <144998976+kobayashi-maru1701@users.noreply.github.com>
2026-08-25 22:23:12 +0200

    Update version.h

--- include/version.h
+++ include/version.h
@@ -8,7 +8,7 @@
 #define BUILD_NAME "TeddyCloud"
 
 #ifndef BUILD_VERSION
-#define BUILD_VERSION "vX.X.X"
+#define BUILD_VERSION "v0.7.0"
 #endif
 
 #ifndef BUILD_GIT_SHORT_SHA
@@ -43,7 +43,7 @@
 #define WEB_NAME "TeddyCloud Web"
 
 #ifndef WEB_VERSION
-#define WEB_VERSION "vX.X.X"
+#define WEB_VERSION "v0.7.0"
 #endif
 
 #ifndef WEB_GIT_SHORT_SHA
